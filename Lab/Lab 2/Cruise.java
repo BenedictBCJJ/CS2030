10042626c9c2a8d4e0e9994a39138905296b452f
BenedictBCJJ <52716670+BenedictBCJJ@users.noreply.github.com>
2020-02-04 23:51:52 +0800

    Added test 6 and java style

--- Lab/Lab 2/Cruise.java
+++ Lab/Lab 2/Cruise.java
@@ -1,38 +1,35 @@
-import java.lang.String;
-
-
 public class Cruise {
-	final String identifier;
-	final int arrival;
-	final int loaders;
-	final int serviceTime;
+    private final String identifier;
+    private final int arrival;
+    private final int loaders;
+    private final int serviceTime;
 
-	public Cruise(String identifier, int arrival, int loaders, int serviceTime) {
-		this.identifier = identifier;
-		this.arrival = arrival;
-		this.loaders = loaders;
-		this.serviceTime = serviceTime;
-	}
+    public Cruise(String identifier, int arrival, int loaders, int serviceTime) {
+        this.identifier = identifier;
+        this.arrival = arrival;
+        this.loaders = loaders;
+        this.serviceTime = serviceTime;
+    }
 
-	@Override
-	public String toString() {
-		return String.format(identifier + "@%04d",arrival);
-	}
+    @Override
+    public String toString() {
+        return String.format(identifier + "@%04d",arrival);
+    }
 
-	public int getArrivalTime() {
-		int minutes = this.arrival % 100;
-		int hours = this.arrival / 100;
-		int totalMinutes = hours * 60 + minutes;
-		return totalMinutes;
-	}
+    public int getArrivalTime() {
+        int minutes = this.arrival % 100;
+        int hours = this.arrival / 100;
+        int totalMinutes = hours * 60 + minutes;
+        return totalMinutes;
+    }
 
-	public int getNumOfLoadersRequired() {
-		return this.loaders;
-	}
+    public int getNumOfLoadersRequired() {
+        return this.loaders;
+    }
 
-	public int getServiceCompletionTime() {
-		int arrival_time = this.getArrivalTime();
-		return arrival_time + serviceTime;
-	}
+    public int getServiceCompletionTime() {
+        int arrivalTime = this.getArrivalTime();
+        return arrivalTime + serviceTime;
+    }
 }
 
